1cb09b329e06e347c8463c12008da572fece86f7
Caio Bozato <caiobozato@gmail.com>
2017-08-29 11:39:52 -0300

    Adicionada máscara no campo de telefone

--- js/script.js
+++ js/script.js
@@ -51,3 +51,17 @@ $(document).ready(function(){
         });
     });
 });
+
+//****************Máscara telefone*******************//
+$(document).ready(function(){
+    var SPMaskBehavior = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+      },
+      spOptions = {
+        onKeyPress: function(val, e, field, options) {
+            field.mask(SPMaskBehavior.apply({}, arguments), options);
+          }
+      };
+      
+      $('.sp_celphones').mask(SPMaskBehavior, spOptions);
+});
