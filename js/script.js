715f1827b42ec730df37126799b95fdffaa3187c
Caio Bozato <caiobozato@gmail.com>
2017-08-29 10:54:29 -0300

    Configurado o envio de email

--- js/script.js
+++ js/script.js
@@ -20,3 +20,34 @@ $(function() {
         $(this).addClass('active');
     });
 });//função para mudar a cor do link ativo
+
+//******************Enviar email**********************//
+
+$(document).ready(function(){
+    $("#form_contato").submit(function(event){
+        // Stop form form submitting normally
+        event.preventDefault();
+
+        //Get some values from elements on the page:
+        var $form = $(this),
+            nome = $form.find("input[id='nome']").val(),
+            telefone = $form.find("input[id='telefone']").val(),
+            email = $form.find("input[id='email']").val(),
+            msg = $form.find("input[id='msg']").val(),
+            url = $form.attr("action");
+        
+        //Send the data using post
+        var posting = $.post(url, {
+            'Nome': nome,
+            'Telefone': telefone,
+            'Email': email,
+            'Mensagem': msg
+
+        });
+
+        // Put the results in a div
+        posting.done(function(data){
+            swal('Mensagem enviada com sucesso!');
+        });
+    });
+});
